fb74c12862c67f87ee03f13e241e243bd1e40375
Jordan <jordannik1211@gmail.com>
2026-04-20 19:17:11 +0700

    Membuat 3 endpoint untuk user

---
+++ src/api/components/users/users-route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const usersController = require('./users-controller');
+const auth = require('../../../core/auth-middleware');
+
+module.exports = (app) => {
+  const router = express.Router();
+
+  //public
+  router.post('/register', usersController.register);
+  router.post('/login', usersController.login);
+
+  //private
+  router.get('/profile', auth, usersController.getProfile);
+}
